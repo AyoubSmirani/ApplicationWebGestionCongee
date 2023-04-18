08413562cbe02ab6f89c66ca85594b3c81eb732d
AyoubSmirani <117396207+AyoubSmirani@users.noreply.github.com>
2023-04-18 17:09:25 +0100

    Update congee.js

--- GestionDesCongee/backend/routes/congee.js
+++ GestionDesCongee/backend/routes/congee.js
@@ -26,7 +26,35 @@ const mystorage = multer.diskStorage(
 const upload = multer({ storage: mystorage })
 
 
-router.post('/postCongee', upload.single('file'), congeeController.postCongee);
+router.post('/postCongee', upload.single('file'), (req,res)=>{
+    var congee = {
+        matricule:req.body.matricule,
+         dateD:req.body.dateDepart,
+        dateF:req.body.dateRetour,
+        type:req.body.type,
+        confirmation:req.body.confirmation,
+        duree:req.body.duree,
+        nomPrenom:req.body.nomPrenom,
+        image:req.body.image,
+        SchefEquipe:req.body.SchefEquipe,
+        EnvoieRh:req.body.EnvoieRh
+    } 
+    req.body.file = filename
+     console.log(congee)
+    db.query("INSERT INTO congee (matricule,nomPrenom,image,dateD,dateF,type,justification,confirmation,duree,SchefEquipe,EnvoieRh) VALUES (?,?,?,?,?,?,?,?,?,?,?)",[congee.matricule,
+        congee.nomPrenom,congee.image
+        ,congee.dateD,congee.dateF
+        ,congee.type,req.body.file,
+        congee.confirmation,congee.duree,
+        congee.SchefEquipe,congee.EnvoieRh],(err,rows,fields)=>{
+         if(!err){
+                res.send({message:'congee is posted'})
+                filename = ''
+         }else{
+             res.send({error:err.message})
+         }
+    })
+});
 
 router.put('/UpdateCongee', congeeController.UpdateCongee);
 
